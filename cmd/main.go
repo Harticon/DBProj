c224815a5652f3329c964ded68584a2b5ea1928c
Vojtech Hromadka <hromadkavojta@gmail.com>
2019-06-20 12:54:51 +0200

    feature(cli):added CLI,(doesn't have impact on app at all)

--- cmd/main.go
+++ cmd/main.go
@@ -8,23 +8,86 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/pkg/profile"
 	"github.com/spf13/viper"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+	"sort"
 )
 
 func main() {
 
-	// todo cli add something that's gonna print
+	// todo CLI add something that's gonna print
+
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "lang, l",
+			Value: "english",
+			Usage: "Language for the greeting",
+		},
+		cli.StringFlag{
+			Name:  "paymentMethod, p",
+			Value: "cash",
+			Usage: "Payment method",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Spoustime funkci complete")
+				return nil
+			},
+		},
+		{
+			Name:    "begin",
+			Aliases: []string{"b"},
+			Usage:   "begin task",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Spoustime funkci begin")
+				return nil
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	app.Action = func(c *cli.Context) error {
+
+		name := "moje nejlepší aplikace na světě"
+
+		if c.NArg() > 0 {
+			name = c.Args().Get(0)
+			fmt.Printf("---------------------------%s\n", name)
+		} else if c.String("lang") == "spanish" {
+			fmt.Println("Hola", name)
+		} else if c.String("lang") == "czech" {
+			fmt.Println("Zdravíčko", name)
+		} else {
+			fmt.Println("Hello", name)
+		}
+		return nil
+
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	govalidator.SetFieldsRequiredByDefault(true)
 
-	defer profile.Start().Stop()
+	//defer profile.Start().Stop()
 
 	viper.SetDefault("db.conn", "prod.db")
 	viper.SetDefault("secret", "secret")
 	viper.SetDefault("hashSecret", "salt&peper")
-
-	fmt.Println(viper.GetString("db.conn"))
 
 	db, err := gorm.Open("sqlite3", viper.GetString("db.conn"))
 	if err != nil {
